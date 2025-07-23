80153fe61fc98c8d9dcdab7688ac647732c684a1
Tana-Tana <phanthanhtan2004@gmail.com>
2025-07-23 16:26:53 +0700

    feat: GameConfig

--- Assets/_Game/Common/GameConfig.cs
+++ Assets/_Game/Common/GameConfig.cs
@@ -5,8 +5,10 @@
         public const string PATH_UI = "UI/"; // duong dan toi cac canvas UI trong Resources
         
         // anim
-        public const string ANIM_TILE_MATCH = "matching"; // ten cua trigger khi tile match
+        public const string ANIM_TILE_APPEAR = "appear"; // ten cua trigger khi tile xuat hien
+        public const string ANIM_TILE_DISAPPEAR = "disappear"; // ten cua trigger khi tile bien mat
         public const string ANIM_TILE_SELECT = "selecting"; // ten cua trigger khi tile duoc chon
+        public const string ANIM_TILE_UNSELECT = "unselecting"; // ten cua trigger khi tile khong duoc chon
         
         // material 
         public const string MATERIAL_LINE_SIZE = "_laser_size"; // size cua line laser
