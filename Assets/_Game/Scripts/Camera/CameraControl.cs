5a18d3349413cd4a592bb2ac998478de1a1d259b
Tana-Tana <phanthanhtan2004@gmail.com>
2025-07-23 16:22:00 +0700

    fix: camera fit other device

--- Assets/_Game/Scripts/Camera/CameraControl.cs
+++ Assets/_Game/Scripts/Camera/CameraControl.cs
@@ -7,20 +7,20 @@ namespace _Game.Scripts.Camera
     {
         [SerializeField] private UnityEngine.Camera mainCamera;
         public UnityEngine.Camera MainCamera => mainCamera;
-        private const float CameraSize = 8.25f; // kich thuoc mac dinh cua camera
+        private const float CameraSize = 7.25f; // kich thuoc mac dinh cua camera
 
         public void FitCameraToBoard(Vector3 bottomLeft, Vector3 topRight, float paddingRatio = 1.5f)
         {
             Vector3 center = (bottomLeft + topRight) / 2; // lay vi tri trung tam cua board
-            mainCamera.transform.position = new Vector3(center.x, center.y, mainCamera.transform.position.z);
+            mainCamera.transform.position = new Vector3(center.x, center.y + 0.5f, mainCamera.transform.position.z);
             
             // size camera
-            /*float width = Mathf.Abs(topRight.x - bottomLeft.x); // tinh khoang cach theo chieu ngang
+            float width = Mathf.Abs(topRight.x - bottomLeft.x); // tinh khoang cach theo chieu ngang
             float height = Mathf.Abs(topRight.y - bottomLeft.y); // tinh khoang cach theo chieu doc
             float aspectRatio = mainCamera.aspect; // ti le khung hinh cua camera
             float size = Mathf.Max((width*paddingRatio)/aspectRatio/2f, (height*paddingRatio)/2f); // paddingRatio de them khoang trang quanh board
             
-            mainCamera.orthographicSize = Mathf.Max(CameraSize, size);*/
+            mainCamera.orthographicSize = Mathf.Max(CameraSize, size);
         }
     }
 }
