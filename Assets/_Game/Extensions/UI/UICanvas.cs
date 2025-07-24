ae420a88ca091c3f9fbdaf74e8f50740766d410e
Tana-Tana <phanthanhtan2004@gmail.com>
2025-07-24 17:36:10 +0700

    feat: UI Panel + refactor vfx select, unslect
    - UI panel: victory, fail, gameplay, mainMenu, setting
    - vfx: match

--- Assets/_Game/Extensions/UI/UICanvas.cs
+++ Assets/_Game/Extensions/UI/UICanvas.cs
@@ -7,24 +7,7 @@ namespace _Game.Extensions.UI
         [SerializeField] private bool isDestroyOnClose = false;
 
         public virtual void SetUp() // khoi tao canvas
-        {
-            // tai tho doi voi man hinh dai
-            RectTransform rect = GetComponent<RectTransform>();
-            float ratio = (float)Screen.width / Screen.height;
-
-            if (ratio > 2.1f)
-            {
-                Vector2 leftBottom = rect.offsetMin;
-                Vector2 rightTop = rect.offsetMax;
-
-                leftBottom.y = 0;
-                rightTop.y = -100f;
-                
-                rect.offsetMin = leftBottom;
-                rect.offsetMax = rightTop;
-            }
-            
-        }
+        { }
 
         public virtual void Open() // mo canvas
         {
